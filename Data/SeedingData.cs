5b945f44a50e2f2f9d28ef8065f1ebce7059e887
Kade Abdul <kaderderk@gmail.com>
2020-05-21 19:16:19 -0500

    updated appsettings

--- Data/SeedingData.cs
+++ Data/SeedingData.cs
@@ -3,16 +3,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MovieWebAppCore.Data
 {
     public static class SeedingData
     {
-        public static void Initializer(BookDbContext context)
+        public static void Initializer(IApplicationBuilder app)
         {
-            context.Database.EnsureCreated();
 
-            if (context.Authors.Any())
+        using (var serviceScope = app.ApplicationServices.CreateScope())
+        
+        {
+           SeedData(serviceScope.ServiceProvider.GetService<BookDbContext>());
+        }
+
+        }
+
+
+        public static void SeedData(BookDbContext context)
+        {
+            System.Console.WriteLine("Applying Migrations......");
+
+            context.Database.Migrate();
+
+           // context.Database.EnsureCreated();
+
+            if (!context.Authors.Any())
             {
                 return;
             }
--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MovieWebAppCore.Abstract;
 using MovieWebAppCore.Models;
+using MovieWebAppCore.Data;
 
 namespace MovieWebAppCore
 {
@@ -26,7 +27,15 @@ namespace MovieWebAppCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<BookDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Connection")));
+            var server = Configuration["DBServer"] ?? "localhost";
+            var port   = Configuration["DBPort"] ?? "1433";
+            var user   = Configuration["DBUser"] ?? "SA";
+            var password= Configuration["DBPassword"] ?? "Marine7815@@";
+            var db     = Configuration["DBDatabase"] ?? "MovieWebDB";
+
+
+            services.AddDbContext<BookDbContext>(options => 
+                options.UseSqlServer($"Server={server},{port};Initial Catalog={db};User ID={user};Password={password}"));
             services.AddTransient<IBookRepository, BookRepository>();
             services.AddTransient<IReviewRepository, ReviewRepository>();
             services.AddTransient<IAuthorRepository, AuthorRepository>();
@@ -60,7 +69,7 @@ namespace MovieWebAppCore
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
-           // Seeding.EnsuredSeed(app);
+           SeedingData.Initializer(app);
         }
     }
 }
