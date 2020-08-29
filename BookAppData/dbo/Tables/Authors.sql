52762e5845a9405bdb2c5c806c183fcba6b733fd
Kade <kaderderk@gmail.com>
2020-08-29 08:26:43 -0500

    Added Data project

---
+++ BookAppData/dbo/Tables/Authors.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Authors] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (MAX) NOT NULL,
+    [BooksCount] INT            NOT NULL,
+    CONSTRAINT [PK_Authors] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
